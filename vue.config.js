93968ac50fbc71444850a43accb6166cb2a04bbd
TaiNguyen <s.tainguyen@okxe.vn>
2022-11-04 16:46:43 +0700

    Init source

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const breakpoints = require("./src/breakpoint.json");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [path.resolve(__dirname, "./src/styles/abstracts/*.styl")],
+    },
+  },
+  css: {
+    extract: false,
+    sourceMap: false,
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/scss/variables/all.scss";
+          @import "@/assets/scss/_functions.scss";
+          @import "@/assets/scss/_mixins.scss";
+          $BREAKPOINT-XS: ${breakpoints.XS};
+          $BREAKPOINT-SM: ${breakpoints.SM};
+          $BREAKPOINT-MD: ${breakpoints.MD};
+          $BREAKPOINT-LG: ${breakpoints.LG};
+          $BREAKPOINT-XL: ${breakpoints.XL};
+          $BREAKPOINT-XXL: ${breakpoints.XXL};
+        `,
+      },
+    },
+  },
+});
